9bb25833bdbb8134ce771b95b980e900e5ad4c94
NasibullinAmir <112939611+NasibullinAmir@users.noreply.github.com>
2023-10-25 09:53:29 +0300

    added kr

--- data.sql
+++ data.sql
@@ -1,17 +1,41 @@
-    insert into activitytype(
-    	  activitytypeid
-        ,name
-        ,sysname
-    )
-    select d.*
-    from (
-    	values(1,'Программа','Program')
-        ,(2,'Подпрограмма','SubProgram')
-        ,(3,'Проект','Project')
-        ,(4,'Контракт','Contract')
-        ,(5,'КТ','Point')
-    ) d(activitytypeid, name, sysname)
-    where not exists(
-    	select 1 from activitytype t
-        where t.activitytypeid = d.activitytypeid
-    );
+INSERT INTO ActivityType (ActivityTypeId, Name)
+VALUES (1, 'Concert'),
+       (2, 'Masterclass'),
+       (3, 'Sports');
+
+INSERT INTO Activity (ActivityId, ParentId, ActivityTypeId, Name, AreaId)
+VALUES (1, NULL, 1, 'Snurov Concert', 1),
+       (2, 1, 2, 'Football Match', 2),
+       (3, 1, 2, 'Hockey match', 3),
+       (4, 2, 1, 'Tupac Concert', 1),
+       (5, 2, 3, 'Mike Tyson training', 3),
+       (6, NULL, 3, 'Modern painting at street', 2);
+
+INSERT INTO Program (ActivityId)
+VALUES (1),
+       (2),
+       (3),
+       (6);
+
+INSERT INTO SubProgram (ActivityId)
+VALUES (4),
+       (5);
+
+INSERT INTO Project (ActivityId)
+VALUES (2),
+       (3),
+       (4),
+       (5);
+
+INSERT INTO Contract (ActivityId, AreaId)
+VALUES (3, 1),
+       (4, 2),
+       (6, 3);
+
+INSERT INTO Point (ActivityId)
+VALUES (1),
+       (2),
+       (3),
+       (4),
+       (5),
+       (6);
--- tables.sql
+++ tables.sql
@@ -1,11 +1,47 @@
-
-create table if not exists activitytype
-(
-    activitytypeid integer not null
-        constraint pk_activitytype
-            primary key,
-    name           text    not null,
-    sysname        text    not null
+create table if not exists ActivityType (
+    ActivityTypeId integer primary key,
+    name varchar not null,
+    sysname varchar not null
 );
 
--- 
+create table if not exists area (
+    AreaId integer primary key,
+    name varchar not null
+);
+
+create table if not exists activity (
+    ActivityId integer primary key,
+    ActivityTypeId integer references ActivityType (ActivityTypeId) not null,
+    code varchar not null,
+    name varchar not null,
+    parentid integer references activity (ActivityId)
+);
+
+create table if not exists contract (
+    contractid integer primary key references activity (ActivityId),
+    areaid integer references area (areaid)
+);
+
+create table if not exists program (
+    programid integer primary key references activity (ActivityId),
+    indexnum integer,
+    yearstart integer,
+    yearfinish integer
+);
+
+create table if not exists point (
+    pointid integer primary key references activity (ActivityId),
+    plandate date not null,
+    factdate date
+);
+
+
+create table if not exist subprogram (
+    subprogramid integer primary key references activity (activityid),
+    indexnum integer
+);
+
+create table if not exist project (
+    projectid integer primary key references activity (activityid),
+    targetdescr varchar
+);
